2bf589cfca732f622b7f441e879a5227b392af3f
Adithya Ramesh <38559664+adi3e08@users.noreply.github.com>
2023-01-05 21:29:52 +0530

    Add files via upload

--- env/acrobot/acrobot.py
+++ env/acrobot/acrobot.py
@@ -1,1 +1,86 @@
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import pygame
+import numpy as np
+from env import rewards
+from env.utils import rect_points, wrap, basic_check
+from env.base import BaseEnv
 
+class acrobot(BaseEnv):
+    def __init__(self, mle):
+        m1 = 0.1
+        self.l1 = 1
+        self.r1 = self.l1/2
+        I1 = m1 * self.l1**2 / 12
+        m2 = 0.1
+        self.l2 = 1
+        self.r2 = self.l2/2
+        I2 = m2 * self.l2**2 / 12
+        g = 9.8
+        super(acrobot, self).__init__(name = "acrobot",
+                                       n = 2,
+                                       obs_size = 6,
+                                       action_size = 1,
+                                       inertials = [m1,self.l1,self.r1,I1,m2,self.l2,self.r2,I2,g],
+                                       dt = 0.01,
+                                       a_scale = np.array([1.75]),
+                                       mle = mle)
+
+    def wrap_state(self):
+        self.state[:2] = wrap(self.state[:2])
+
+    def reset_state(self):
+        self.state = np.array([np.pi + 0.01*np.random.randn(),\
+                               0.01*np.random.randn(),\
+                               0,\
+                               0])
+        self.wrap_state()
+
+    def get_obs(self):
+        return np.array([np.cos(self.state[0]),np.sin(self.state[0]),\
+                        np.cos(self.state[1]),np.sin(self.state[1]),\
+                        self.state[2],\
+                        self.state[3]\
+                        ])
+
+    def get_reward(self):
+        upright = (np.array([np.cos(self.state[0]), np.cos(self.state[0]+self.state[1])])+1)/2
+
+        qdot = self.state[self.n:]
+        ang_vel = np.array([qdot[0],qdot[0]+qdot[1]])        
+        small_velocity = rewards.tolerance(ang_vel, margin=self.ang_vel_limit).min()
+        small_velocity = (1 + small_velocity) / 2
+        
+        reward = upright.mean() * small_velocity
+
+        self.reward_breakup.append([upright.mean(),small_velocity])
+
+        return reward
+
+    def get_power(self, a, sdot):
+        return np.array([a[0]*sdot[1]])
+
+    def draw(self):
+        offset = [250, 250]
+        scaling = 75
+        height = 0.15 
+
+        joint1 = offset
+        link1_center = [(self.l1/2)*np.sin(self.state[0]),\
+                      (self.l1/2)*np.cos(self.state[0])]
+        link1_points = rect_points(link1_center, self.l1, height, np.pi/2-self.state[0],scaling,offset) 
+        joint2 = [offset[0]+scaling*(self.l1)*np.sin(self.state[0]),\
+                       offset[1]-scaling*(self.l1)*np.cos(self.state[0])]
+        link2_center = [(self.l1)*np.sin(self.state[0])+\
+                      (self.l2/2)*np.sin(self.state[0]+self.state[1]),\
+                      (self.l1)*np.cos(self.state[0])+\
+                      (self.l2/2)*np.cos(self.state[0]+self.state[1])]
+        link2_points = rect_points(link2_center, self.l2, height, np.pi/2-self.state[0]-self.state[1],scaling,offset)
+
+        pygame.draw.polygon(self.screen, (72,209,204), link1_points) # medium turquoise
+        pygame.draw.circle(self.screen,(255,69,0), joint1, scaling*height/1.8) # orange red
+        pygame.draw.polygon(self.screen, (72,209,204), link2_points) # medium turquoise
+        pygame.draw.circle(self.screen,(255,69,0), joint2, scaling*height/1.8) # orange red
+
+if __name__ == '__main__':
+    basic_check("acrobot",0)
