45af17b9f2dffd08aa014c4b0b34f7c5809c26a4
Adithya Ramesh <38559664+adi3e08@users.noreply.github.com>
2023-07-16 02:31:09 +0530

    Update env

--- env/reacher/reacher.py
+++ env/reacher/reacher.py
@@ -3,44 +3,54 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 import numpy as np
 from env import rewards
-from env.utils import rect_points, wrap, basic_check
+from env.utils import rect_points, wrap, basic_check, pygame_transform
 from env.base import BaseEnv
 
 class reacher(BaseEnv):
-    def __init__(self, mle):
+    def __init__(self):
         m1 = 0.1
-        self.l1 = 1
-        self.r1 = self.l1/2
-        I1 = m1 * self.l1**2 / 12
+        l1 = 1
+        r1 = l1/2
+        I1 = m1 * l1**2 / 12
+        
         m2 = 0.1
-        self.l2 = 1
-        self.r2 = self.l2/2
-        I2 = m2 * self.l2**2 / 12
+        l2 = 1
+        r2 = l2/2
+        I2 = m2 * l2**2 / 12
+        
         g = 0.0
+        
+        m = [m1,m2]
+        l = [l1,l2]
+        r = [r1,r2]
+        I = [I1,I2]
         super(reacher, self).__init__(name = "reacher",
-                                       n = 2,
-                                       obs_size = 6,
-                                       action_size = 2,
-                                       inertials = [m1,self.l1,self.r1,I1,m2,self.l2,self.r2,I2,g],
-                                       dt = 0.02,
-                                       a_scale = np.array([0.1,0.1]),
-                                       mle = mle)
+                                      n = 2,
+                                      obs_size = 6,
+                                      action_size = 2,
+                                      inertials = m+l+r+I+[g],
+                                      a_scale = np.array([0.1,0.1]))
+        self.dt = 0.02
+        self.goal_position = np.array([0,2])
 
     def wrap_state(self):
         self.state[:2] = wrap(self.state[:2])
 
     def reset_state(self):
-        self.state = np.array([np.pi + 0.01*np.random.randn(),\
-                               0.01*np.random.randn(),\
-                               0,\
+        self.state = np.array([np.pi + 0.01*np.random.randn(),
+                               0.01*np.random.randn(),
+                               0,
                                0])
-        self.wrap_state()
+
+    def get_A(self, a):
+        a_1, a_2 = np.clip(a, -1.0, 1.0)*self.a_scale
+        return np.array([a_1,a_2])
 
     def get_obs(self):
-        return np.array([np.cos(self.state[0]),np.sin(self.state[0]),\
-                        np.cos(self.state[1]),np.sin(self.state[1]),\
-                        self.state[2],\
-                        self.state[3]\
+        return np.array([np.cos(self.state[0]),np.sin(self.state[0]),
+                        np.cos(self.state[1]),np.sin(self.state[1]),
+                        self.state[2],
+                        self.state[3]
                         ])
 
     def get_reward(self):
@@ -53,48 +63,21 @@ class reacher(BaseEnv):
         
         reward = upright.mean() * small_velocity
 
-        self.reward_breakup.append([upright.mean(),small_velocity])
-
         return reward
 
-    def get_power(self, a, sdot):
-        return np.array([a[0]*sdot[0]+a[1]*sdot[1]])
-
     def draw(self):
-        offset = [250, 250]
-        scaling = 75
-        height = 0.15 
+        pygame.draw.circle(self.screen,'slateblue1', [self.offset[0]+self.scaling*self.goal_position[0],
+                                                      self.offset[1]-self.scaling*self.goal_position[1]], self.scaling*self.link_width)
 
-        dis_threshold = 0.15
-        theta1  = 0
-        theta2  = 0
-        goal_position = np.array([self.l1*np.sin(theta1)+self.l2*np.sin(theta1+theta2),\
-                                  self.l1*np.cos(theta1)+self.l2*np.cos(theta1+theta2)])
-        pygame.draw.circle(self.screen,(0,255,0), [offset[0]+scaling*goal_position[0],\
-                                                   offset[1]-scaling*goal_position[1]], scaling*dis_threshold)
+        centers, joints, angles = self.geo
 
-        joint1 = offset
-        link1_center = [(self.l1/2)*np.sin(self.state[0]),\
-                      (self.l1/2)*np.cos(self.state[0])]
-        link1_points = rect_points(link1_center, self.l1, height, np.pi/2-self.state[0],scaling,offset) 
-        joint2 = [offset[0]+scaling*(self.l1)*np.sin(self.state[0]),\
-                       offset[1]-scaling*(self.l1)*np.cos(self.state[0])]
-        link2_center = [(self.l1)*np.sin(self.state[0])+\
-                      (self.l2/2)*np.sin(self.state[0]+self.state[1]),\
-                      (self.l1)*np.cos(self.state[0])+\
-                      (self.l2/2)*np.cos(self.state[0]+self.state[1])]
-        link2_points = rect_points(link2_center, self.l2, height, np.pi/2-self.state[0]-self.state[1],scaling,offset)
+        for j in range(self.n):
+            link_points = rect_points(centers[j], self.link_length, self.link_width, angles[j,0],self.scaling,self.offset)
+            pygame.draw.polygon(self.screen, self.link_color, link_points)
+        
+            joint_point = pygame_transform(joints[j],self.scaling,self.offset)
+            pygame.draw.circle(self.screen, self.joint_color, joint_point, self.scaling*self.joint_radius)
 
-        pygame.draw.polygon(self.screen, (72,209,204), link1_points) # medium turquoise
-        pygame.draw.circle(self.screen,(255,69,0), joint1, scaling*height/1.8) # orange red
-        pygame.draw.polygon(self.screen, (72,209,204), link2_points) # medium turquoise
-        pygame.draw.circle(self.screen,(255,69,0), joint2, scaling*height/1.8) # orange red
-
-        end_point = [offset[0]+scaling*((self.l1)*np.sin(self.state[0])+\
-                      (self.l2)*np.sin(self.state[0]+self.state[1])),\
-                      offset[1]-scaling*((self.l1)*np.cos(self.state[0])+\
-                      (self.l2)*np.cos(self.state[0]+self.state[1]))]
-        pygame.draw.circle(self.screen,'slateblue1', end_point, scaling*height/1.8) # orange red 
 
 if __name__ == '__main__':
     basic_check("reacher",0)
